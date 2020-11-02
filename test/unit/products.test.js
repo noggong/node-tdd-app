7d05476ba529c6527761581f2d34ec44f6ffb7d0
noggong <noggong@gamil.com>
2020-11-02 23:23:09 +0900

    node.js 앱을 테스트하기 위한 Jest 설정

--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -1,5 +1,7 @@
 const productController = require("../../controllers/product");
 const productModel = require("../../models/Product");
+const httpMocks = require("node-mocks-http");
+const newProduct = require("../data/new-product.json");
 
 //  모델 create해서 진짜로 insert되는 것을 방지하기 위해 mock 함수를 생성
 productModel.create = jest.fn();
@@ -10,7 +12,13 @@ describe("Product Controller Create", () => {
     });
 
     it("Should call ProductModel.create", () => {
-        productController.createProduct();
-        expect(productModel.create).toBeCalled();
+        let req = httpMocks.createRequest();
+        let res = httpMocks.createResponse();
+        let next = null;
+        req.body = newProduct;
+        productController.createProduct(req, res, next);
+
+        // productController.createProduct 호출시 productModel.create가 호출 되었는지 확인
+        expect(productModel.create).tobeCalledWith(req.body);
     });
 });
